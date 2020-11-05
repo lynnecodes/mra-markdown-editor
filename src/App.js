277696f12066e94bb19fbc44f9cd56e674278d86
Lynne Buencamino <Lynne@lynnes-mbp.lan>
2020-11-05 13:03:39 -0800

    step-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const [markdown, setMarkdown] = useState('# sup');
+
+
   return (
     <div className="app">
-      <textarea />
+      <textarea value={markdown} />
 
-      <div className="preview" />
+      <div className="preview">Render markdown here</div>
     </div>
   );
 }
